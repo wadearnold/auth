7947a6b3970fb195d37c95706ee23a4475e811b4
Adam Shannon <adamkshannon@gmail.com>
2018-09-12 18:13:13 -0500

    pkg/buntdbclient: add GetByUserId, add docs/test debug

--- pkg/buntdbclient/client.go
+++ pkg/buntdbclient/client.go
@@ -4,6 +4,14 @@
 
 // buntdbclient implements ClientStore from gopkg.in/oauth2.v3
 // using BuntDB (https://github.com/tidwall/buntdb).
+//
+// A few extra operations have been added though, such as Set and
+// GetByUserId. These were needed for ourusecase as we're mutating
+// the oauth cleints.
+//
+// Tests can be ran with a database in the package dir, just add -debug
+// as a flag to 'go test'.
+// The local database will be deleted before tests are ran each time.
 package buntdbclient
 
 import (
@@ -20,26 +28,35 @@ var (
 	DefaultTTL time.Duration = 24 * time.Hour
 )
 
-func New(path string) (oauth2.ClientStore, error) {
+// New initializes a new BuntDB database with any indicies needed
+// for the Get* operations.
+func New(path string) (*ClientStore, error) {
 	db, err := buntdb.Open(path)
 	if err != nil {
 		return nil, err
 	}
+
+	// Migrations
+	db.CreateIndex("user_id", "*", buntdb.IndexJSON("UserID")) // ScanByUserId
+
 	return &ClientStore{
 		db: db,
 	}, nil
 }
 
+// ClientStore wraps oauth2.ClientStore
 type ClientStore struct {
 	oauth2.ClientStore
 
 	db *buntdb.DB
 }
 
+// Close shuts down connections to the underlying database
 func (cs *ClientStore) Close() error {
 	return cs.db.Close()
 }
 
+// GetById returns an oauth2.ClientInfo if the ID matches id.
 func (cs *ClientStore) GetByID(id string) (oauth2.ClientInfo, error) {
 	var cli models.Client
 	cli.ID = id
@@ -71,6 +88,7 @@ func (cs *ClientStore) GetByID(id string) (oauth2.ClientInfo, error) {
 	return &cli, nil
 }
 
+// Set writes the oauth2.ClientInfo to the underlying database.
 func (cs *ClientStore) Set(id string, cli oauth2.ClientInfo) error {
 	if inc := cli.GetID(); id != inc {
 		return fmt.Errorf("ClientStore: id's don't match, id=%s and cli=%s", id, inc)
@@ -96,4 +114,51 @@ func (cs *ClientStore) Set(id string, cli oauth2.ClientInfo) error {
 		return fmt.Errorf("problem updating %s: %v", id, err)
 	}
 	return nil
+}
+
+// GetByUserID returns an array of oauth2.ClientInfo which have a UserID mathcing
+// userId.
+// If return values are nil that means no matching records were found.
+func (cs *ClientStore) GetByUserID(userId string) ([]oauth2.ClientInfo, error) {
+	keys := make(map[string]bool, 0)
+
+	err := cs.db.View(func(tx *buntdb.Tx) error {
+		return tx.AscendEqual("user_id", userId, func(k,v string) bool {
+			if v == userId {
+				keys[k] = true
+			}
+			if k > userId {
+				// quit iterating once we go "past" userId
+				return false
+			}
+			return true
+		})
+	})
+	if err != nil {
+		fmt.Println("A")
+		return nil, err
+	}
+
+	// Grab each ClientInfo now
+	var accum []oauth2.ClientInfo
+	for k,_ := range keys {
+		ci, _ := cs.GetByID(k)
+		if ci != nil {
+			accum = append(accum, ci)
+		}
+	}
+	if len(accum) == 0 {
+		return nil, nil
+	}
+	return accum, nil
+}
+
+// DeleteByID removes the oauth2.ClientInfo for the provided id.
+func (cs *ClientStore) DeleteByID(id string) error {
+	return cs.db.Update(func(tx *buntdb.Tx) (e error) {
+		tx.Delete(fmt.Sprintf("%s-secret", id))
+		tx.Delete(fmt.Sprintf("%s-domain", id))
+		_, err := tx.Delete(fmt.Sprintf("%s-user-id", id))
+		return err
+	})
 }
--- pkg/buntdbclient/client_test.go
+++ pkg/buntdbclient/client_test.go
@@ -10,19 +10,62 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"flag"
 
 	"gopkg.in/oauth2.v3/models"
 )
 
-func TestClientStore(t *testing.T) {
+var (
+	flagDebug = flag.Bool("debug", false, "Create db inside project dir for tests")
+)
+
+type testCS struct {
+	*ClientStore
+
+	// temp dir used
+	dir string
+}
+func (cs *testCS) cleanup() error {
+	if cs == nil {
+		return nil
+	}
+	err := cs.Close()
+	if cs.dir != "" {
+		os.RemoveAll(cs.dir)
+	}
+	return err
+}
+
+func makeCS(t *testing.T) (*testCS, error) {
+	t.Helper()
+
+	filename := "client_test.db"
+	if *flagDebug {
+		os.Remove(filename)
+		db, err := New(filename)
+		if err != nil {
+			return nil, err
+		}
+		return &testCS{db, ""}, nil
+	}
+
 	dir, err := ioutil.TempDir("", "moov-auth-client")
+	if err != nil {
+		return nil, err
+	}
+	db, err := New(filepath.Join(dir, filename))
+	if err != nil {
+		return nil, err
+	}
+	return &testCS{db, dir}, nil
+}
+
+func TestClientStore(t *testing.T) {
+	cs, err := makeCS(t)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(dir)
-
-	db, _ := New(filepath.Join(dir, "client_test.db"))
-	cs, _ := db.(*ClientStore)
+	defer cs.cleanup()
 
 	id := "moov"
 
@@ -62,5 +105,101 @@ func TestClientStore(t *testing.T) {
 	}
 	if cli.GetUserID() != "userId" {
 		t.Errorf("got %s", cli.GetUserID())
+	}
+}
+
+func TestClientStore__scan(t *testing.T) {
+	cs, err := makeCS(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cs.cleanup()
+
+	id, userId := "moov", "user-id"
+
+	// scan nothing
+	results, err := cs.GetByUserID(userId)
+	if results != nil || err != nil {
+		t.Errorf("got results=%v, err=%#v", results, err)
+	}
+	if v := len(results); v != 0 {
+		t.Errorf("got %d", v)
+	}
+
+	// write something
+	err = cs.Set(id, &models.Client{
+		ID:     id,
+		Secret: "secret",
+		Domain: "domain",
+		UserID: userId,
+	})
+	if err != nil {
+		t.Errorf("got %v", err)
+	}
+	err = cs.Set(id+"2", &models.Client{
+		ID:     id+"2",
+		Secret: "secret",
+		Domain: "domain",
+		UserID: userId+"2",
+	})
+	if err != nil {
+		t.Errorf("got %v", err)
+	}
+	err = cs.Set("other-id", &models.Client{
+		ID:     "other-id",
+		Secret: "secret",
+		Domain: "domain",
+		UserID: "other-user",
+	})
+	if err != nil {
+		t.Errorf("got %v", err)
+	}
+
+	// scan something
+	results, err = cs.GetByUserID(userId)
+	if err != nil {
+		t.Error(err)
+	}
+	if v := len(results); v != 1 {
+		t.Errorf("got %d", v)
+	}
+}
+
+func TestClientStore__delete(t *testing.T) {
+	cs, err := makeCS(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cs.cleanup()
+
+	id := "moov"
+
+	// get nothing
+	cli, err := cs.GetByID(id)
+	if err == nil || !strings.Contains(err.Error(), "not found") {
+		t.Errorf("got %#v", err)
+	}
+
+	// set something
+	cs.Set(id, &models.Client{
+		ID:     id,
+		Secret: "secret",
+		Domain: "domain",
+		UserID: "userId",
+	})
+
+	// get something
+	cli, err = cs.GetByID(id)
+	if err != nil || cli == nil {
+		t.Errorf("got cli=%v, err=%#v", cli, err)
+	}
+
+	// delete
+	cs.DeleteByID(id)
+
+	// get nothing :-(
+	cli, err = cs.GetByID(id)
+	if err == nil || !strings.Contains(err.Error(), "not found") {
+		t.Errorf("got %#v", err)
 	}
 }
