e3c0dc172629850800978b2500cf349e56f4cf39
Adam Shannon <adamkshannon@gmail.com>
2018-09-13 20:04:55 -0500

    admin: enable all pprof profiles by default

---
+++ admin/admin.go
@@ -0,0 +1,22 @@
+// Copyright 2018 The ACH Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"runtime"
+)
+
+// Init is the entrypoint into the admin package. It will
+// configure the runtime.
+func Init() error {
+	if pprofProfileEnabled("block", true) {
+		runtime.SetBlockProfileRate(1)
+	}
+	if pprofProfileEnabled("mutex", true) {
+		runtime.SetMutexProfileFraction(1)
+	}
+
+	return nil
+}
--- admin/http.go
+++ admin/http.go
@@ -8,8 +8,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/pprof"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -55,45 +53,6 @@ func (s *Server) Shutdown() {
 	s.svc.Shutdown(nil)
 }
 
-// pprofHandlers is a map which holds configuration for
-// serving various pprof handlers on the admin servlet.
-//
-// We only wnat to expose on the admin servlet because these
-// profiles/dumps can contain sensitive info (i.e. password hash, email)
-// or alter the app performance.
-//
-// These values can be overridden by setting the appropiate PPROF_*
-// environment variable.
-//
-// TODO(adam): generate readme.md env vars from this
-var pprofHandlers = map[string]bool{
-	"allocs":       true,
-	"block":        true,
-	"cmdline":      true,
-	"goroutine":    true,
-	"heap":         true,
-	"mutex":        true,
-	"profile":      true,
-	"threadcreate": false,
-	"trace":        false,
-}
-
-// checkEnabled calls out to os.Getenv with the following pattern:
-//  PPROF_$name where $name is uppercase
-//
-// A string of "yes" returns true, and "no" returns false, otherwise
-// zero is returned.
-func checkEnabled(name string, zero bool) bool {
-	v := os.Getenv(fmt.Sprintf("PPROF_%s", strings.ToUpper(name)))
-	switch strings.ToLower(v) {
-	case "yes":
-		return true
-	case "no":
-		return false
-	}
-	return zero
-}
-
 func handler() http.Handler {
 	r := mux.NewRouter()
 
@@ -103,7 +62,7 @@ func handler() http.Handler {
 	// add all pprof handlers we've configured
 	r.HandleFunc("/debug/pprof/", pprof.Index)
 	for k, add := range pprofHandlers {
-		if checkEnabled(k, add) {
+		if pprofProfileEnabled(k, add) {
 			r.Handle(fmt.Sprintf("/debug/pprof/%s", k), pprof.Handler(k))
 		}
 	}
